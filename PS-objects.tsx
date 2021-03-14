5385be01476aff2a36fa70241e85a37926cea0f0
maroneze <maroneze@users.noreply.github.com>
2021-03-14 19:02:59 +0000

    Deploying to gh-pages from master @ 8ec59c27a9f9336fbd751056f683d205b0df3cca 🚀

--- PS-objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="PS-objects" tilewidth="32" tileheight="32" tilecount="8" columns="8">
- <image source="../objectstileset.png" width="256" height="32"/>
-</tileset>
